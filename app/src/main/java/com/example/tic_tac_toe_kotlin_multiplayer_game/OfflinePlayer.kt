0720c44dc32fca02c88b7d8ef6f3f985b0536dd6
edo-edo <ediberidze99@gmail.com>
2021-01-11 23:13:41 +0400

    Two offline player starting design

--- app/src/main/java/com/example/tic_tac_toe_kotlin_multiplayer_game/OfflinePlayer.kt
+++ app/src/main/java/com/example/tic_tac_toe_kotlin_multiplayer_game/OfflinePlayer.kt
@@ -9,23 +9,20 @@ import android.widget.Button
 import androidx.navigation.fragment.findNavController
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A simple [Fragment] subclass is class which handle two player game on one phone.
  */
-class FirstFragment : Fragment() {
+class OfflinePlayer : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_offline_player, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 }
