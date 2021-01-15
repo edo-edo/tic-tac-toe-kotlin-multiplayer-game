6af2b32604830eddf527befc55138d23b605a653
ikakooo <i_chkhitunidze@cu.edu.ge>
2021-01-16 02:16:24 +0400

    add registrations components

---
+++ app/src/main/java/com/example/tic_tac_toe_kotlin_multiplayer_game/ui/multiPlayer/online/RegisterInToOnlineGameFragment.kt
@@ -0,0 +1,24 @@
+package com.example.tic_tac_toe_kotlin_multiplayer_game.ui.multiPlayer.online
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.example.tic_tac_toe_kotlin_multiplayer_game.R
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class RegisterInToOnlineGameFragment : Fragment(R.layout.fragment_register_in_to_online_game) {
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
