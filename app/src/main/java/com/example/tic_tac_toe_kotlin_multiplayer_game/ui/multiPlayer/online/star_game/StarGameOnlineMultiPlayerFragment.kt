d5c78d3ba458d9f777038454cf0e0f2331819b80
ikakooo <i_chkhitunidze@cu.edu.ge>
2021-01-24 16:10:31 +0400

    firebase integration

--- app/src/main/java/com/example/tic_tac_toe_kotlin_multiplayer_game/ui/multiPlayer/online/star_game/StarGameOnlineMultiPlayerFragment.kt
+++ app/src/main/java/com/example/tic_tac_toe_kotlin_multiplayer_game/ui/multiPlayer/online/star_game/StarGameOnlineMultiPlayerFragment.kt
@@ -1,16 +1,14 @@
 package com.example.tic_tac_toe_kotlin_multiplayer_game.ui.multiPlayer.online.star_game
 
 import android.os.Bundle
-import android.util.Log
 import android.util.Log.d
-import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tic_tac_toe_kotlin_multiplayer_game.R
-import com.example.tic_tac_toe_kotlin_multiplayer_game.extensions.toEditable
 import com.example.tic_tac_toe_kotlin_multiplayer_game.tools.SharedPrefManager.Companion.getInstance
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
@@ -18,19 +16,18 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class StarGameOnlineMultiPlayerFragment : Fragment(R.layout.fragment_star_game_online_multi_player) {
+class StarGameOnlineMultiPlayerFragment :
+    Fragment(R.layout.fragment_star_game_online_multi_player) {
 
-    private  var playersList = mutableListOf<PlayerModel>()
-    private  lateinit var playersAdapter: OnlinePlayersListAdapter
+    private var playersList = mutableListOf<PlayerModel>()
+    private lateinit var playersAdapter: OnlinePlayersListAdapter
 
     lateinit var auth: FirebaseAuth
-
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -38,9 +35,31 @@ class StarGameOnlineMultiPlayerFragment : Fragment(R.layout.fragment_star_game_o
         val onlinePlayerName = view.findViewById<EditText>(R.id.online_player_name)
         val saveButton = view.findViewById<Button>(R.id.save_button)
 
+        auth = FirebaseAuth.getInstance()
+        // auth.currentUser?.uid
+        val onlineUser = auth.currentUser
+        val uID = onlineUser?.uid
+        val currentEmail = onlineUser?.email
+
+        // Write a message to the database
+        val database = Firebase.database
+
+        val reference = database.reference
+
+        val myRef = database.getReference("Players/$uID")
+        val myRefActiveStatus = database.getReference("Players_ActiveStatus")
 
 
-        context?.let { getInstance(it)?.saveUser(PlayerModel("irakli Chkhitunidze",null,null,null)) }
+        context?.let {
+            getInstance(it)?.saveUser(
+                PlayerModel(
+                    "irakli Chkhitunidze",
+                    null,
+                    null,
+                    null
+                )
+            )
+        }
         view.findViewById<RecyclerView>(R.id.online_Players_RecyclerView).apply {
             playersAdapter = OnlinePlayersListAdapter(playersList,
                 object : ItemClickListener {
@@ -55,74 +74,100 @@ class StarGameOnlineMultiPlayerFragment : Fragment(R.layout.fragment_star_game_o
             adapter = playersAdapter
         }
 
-        playersList.add(PlayerModel("fgdgfdg",null,null,null))
-        playersList.add(PlayerModel("null",null,"irakli Chkhitunidze",null))
-        playersList.add(PlayerModel("Giorgi ediberidze",null,null,null))
-        playersList.add(PlayerModel("fgdgfdg",null,null,null))
-        playersList.add(PlayerModel("null",null,"irakli Chkhitunidze",null))
-        playersList.add(PlayerModel("Giorgi ediberidze",null,null,null))
-        playersList.add(PlayerModel("fgdgfdg",null,null,null))
-        playersList.add(PlayerModel("null",null,"irakli Chkhitunidze",null))
-        playersList.add(PlayerModel("Giorgi ediberidze",null,null,null))
-        playersList.add(PlayerModel("fgdgfdg",null,null,null))
-        playersList.add(PlayerModel("null",null,"irakli Chkhitunidze",null))
-        playersList.add(PlayerModel("Giorgi ediberidze",null,null,null))
-        playersList.add(PlayerModel("fgdgfdg",null,null,null))
-        playersList.add(PlayerModel("null",null,"irakli Chkhitunidze",null))
-        playersList.add(PlayerModel("Giorgi ediberidze",null,null,null))
-        playersList.add(PlayerModel("fgdgfdg",null,null,null))
-        playersList.add(PlayerModel("null",null,"irakli Chkhitunidze",null))
-        playersList.add(PlayerModel("Giorgi ediberidze",null,null,null))
-        playersList.add(PlayerModel("fgdgfdg",null,null,null))
-        playersList.add(PlayerModel("null",null,"irakli Chkhitunidze",null))
-        playersList.add(PlayerModel("Giorgi ediberidze",null,null,null))
-        playersList.add(PlayerModel("fgdgfdg",null,null,null))
-        playersList.add(PlayerModel("null",null,"irakli Chkhitunidze",null))
-        playersList.add(PlayerModel("Giorgi ediberidze",null,null,null))
+        activeUsers(database)
+        fetchCurrentUser()
+        // fetchCurrentPlayers()
+
+//        playersList.add(PlayerModel("fgdgfdg", null, null, null))
+//        playersList.add(PlayerModel("null", null, "irakli Chkhitunidze", null))
+//        playersList.add(PlayerModel("Giorgi ediberidze", null, null, null))
+//        playersList.add(PlayerModel("fgdgfdg", null, null, null))
+//        playersList.add(PlayerModel("null", null, "irakli Chkhitunidze", null))
+//        playersList.add(PlayerModel("Giorgi ediberidze", null, null, null))
+//        playersList.add(PlayerModel("fgdgfdg", null, null, null))
+//        playersList.add(PlayerModel("null", null, "irakli Chkhitunidze", null))
+//        playersList.add(PlayerModel("Giorgi ediberidze", null, null, null))
+//        playersList.add(PlayerModel("fgdgfdg", null, null, null))
+//        playersList.add(PlayerModel("null", null, "irakli Chkhitunidze", null))
+//        playersList.add(PlayerModel("Giorgi ediberidze", null, null, null))
+//        playersList.add(PlayerModel("fgdgfdg", null, null, null))
+//        playersList.add(PlayerModel("null", null, "irakli Chkhitunidze", null))
+//        playersList.add(PlayerModel("Giorgi ediberidze", null, null, null))
+//        playersList.add(PlayerModel("fgdgfdg", null, null, null))
+//        playersList.add(PlayerModel("null", null, "irakli Chkhitunidze", null))
+//        playersList.add(PlayerModel("Giorgi ediberidze", null, null, null))
+//        playersList.add(PlayerModel("fgdgfdg", null, null, null))
+//        playersList.add(PlayerModel("null", null, "irakli Chkhitunidze", null))
+//        playersList.add(PlayerModel("Giorgi ediberidze", null, null, null))
+//        playersList.add(PlayerModel("fgdgfdg", null, null, null))
+//        playersList.add(PlayerModel("null", null, "irakli Chkhitunidze", null))
+        playersList.add(PlayerModel("Giorgi ediberidze", null, null, null))
         playersAdapter.notifyDataSetChanged()
 
 
 
-        auth = FirebaseAuth.getInstance()
-       // auth.currentUser?.uid
-        val onlineUser = auth.currentUser
-        val uid = onlineUser?.uid
 
-        val reference = Firebase.database.reference
 
-        val postListener = object : ValueEventListener {
+        reference.child("Players").addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // Get Post object and use the values to update the UI
-               // val post = dataSnapshot.getValue<PlayerModel>()
-                // ...
-d("dsiuhfdsd", dataSnapshot.toString())
-               // onlinePlayerName.text =post?.name?.toEditable()
+                val map = (dataSnapshot.value as Map<*, *>?)
+                val point = map?.get(uID)
+                //  playersList = point as MutableList<PlayerModel>
+//                val sdf = map[uid] as PlayerModel?
+                d("fdfvmapcddwd", map.toString())
+
+                d("fdfvcddwd", point.toString())
+                //d("fdfvcddwd", playersList.toString())
+
+//                d("fdfvcddwd", sdf.toString())
+
+                d("fdfdvd", dataSnapshot.toString())
 
             }
 
-            override fun onCancelled(databaseError: DatabaseError) {
-                // Getting Post failed, log a message
-                Log.d("TAGdgergerg", "loadPost:onCancelled", databaseError.toException())
-                // ...
-            }
-        }
-        reference.addValueEventListener(postListener)
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
 
-
-        // Write a message to the database
-        val database = Firebase.database
-        val myRef = database.getReference("Players/$uid")
+//        val postListener = object : ValueEventListener {
+//            override fun onDataChange(dataSnapshot: DataSnapshot) {
+//                // Get Post object and use the values to update the UI
+//                 val post = (dataSnapshot.value as Map<*, *>?)
+//                // ...
+//                d("dsiuhfdsd", dataSnapshot.toString())
+//                // onlinePlayerName.text =post?.name?.toEditable()
+//
+//            }
+//
+//            override fun onCancelled(databaseError: DatabaseError) {
+//                // Getting Post failed, log a message
+//                d("TAGdgergerg", "loadPost:onCancelled", databaseError.toException())
+//                // ...
+//            }
+//        }
+//        reference.addValueEventListener(postListener)
 
 
         //myRef.setValue(playersList)
 
         saveButton.setOnClickListener {
 
-            myRef.setValue(PlayerModel("chlhktu Chkhitdsfsfsdfsdfunidzeikakooo","gmailikakoonull@Gmail.com",onlinePlayerName.text.toString(),true))
+            myRef.setValue(
+                PlayerModel(
+                    uID,
+                    currentEmail,
+                    onlinePlayerName.text.toString(),
+                    true
+                )
+            )
         }
-
-
-
+        myRefActiveStatus.setValue(
+            PlayerModel(
+                uID,
+                currentEmail,
+                onlinePlayerName.text.toString(),
+                true
+            )
+        )
 
         //        val fistPlayerName = view.findViewById<EditText>(R.id.fist_player_name)
 //        val secondPlayerName = view.findViewById<EditText>(R.id.second_player_name)
@@ -138,5 +183,70 @@ d("dsiuhfdsd", dataSnapshot.toString())
 //                it.myCustomSnackbar("please fill out this fields", R.id.button_second)
 //            }
 //        }
+    }
+
+
+    private fun activeUsers(database: FirebaseDatabase): List<PlayerModel>? {
+        database.reference.child("Players")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val map = (dataSnapshot.value as Map<*, *>?)
+                    d("fdfdvd", dataSnapshot.toString())
+                    map?.forEach {
+                        val model = it.value as Map<*, *>?
+
+                        d("activeStatusX", model?.get("activeStatus").toString())
+                        d("activeStatusX", model?.get("email").toString())
+                        d("activeStatusX", model?.get("name").toString())
+                        d("activeStatusX", model?.get("uid").toString())
+
+                        d("fdfvmapcgfdfgfgdfgddwd", it.key.toString())
+                        d("fdfvmapcfdfgfddwd", it.value.toString())
+                        //  d("fdfvmapcffddwd", (it.value as PlayerModel).toString())
+                    }
+
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {}
+            })
+
+        return null
+    }
+
+
+    private fun fetchCurrentUser() {
+        val uid = auth.currentUser!!.uid
+        val ref = FirebaseDatabase.getInstance().getReference("Players/$uid")
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val currentUser = snapshot.getValue(PlayerModel::class.java)
+
+                d("sdas", currentUser.toString())
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
+
+    private fun fetchCurrentPlayers() {
+        val uid = auth.currentUser!!.uid
+        val ref = FirebaseDatabase.getInstance().getReference("Players")
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+//                val  currentUser = snapshot.getValue(List<PlayerModel::class.java>())
+                val currentUsers =
+                    snapshot.getValue(mutableMapOf<String, PlayerModel>()::class.java)
+
+                d("sdagfds", currentUsers.toString())
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
 }
