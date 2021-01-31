18cb3140e90ebeb4be56a8525245bab509705f6e
edo-edo <ediberidze99@gmail.com>
2021-01-31 18:47:49 +0400

    remove circle when change icon

--- app/src/main/java/com/example/tic_tac_toe_kotlin_multiplayer_game/ui/themes/ThemesActivity.kt
+++ app/src/main/java/com/example/tic_tac_toe_kotlin_multiplayer_game/ui/themes/ThemesActivity.kt
@@ -3,7 +3,6 @@ package com.example.tic_tac_toe_kotlin_multiplayer_game.ui.themes
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
